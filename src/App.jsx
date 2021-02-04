c7d8c8c8b3f50528ee7cbf4e1e6f7b0dbef6fd86
Hasibur Rahman <hr.hasan.info@gmail.com>
2021-02-05 00:01:37 +0600

    Updating a small change

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+    return (<div className="App" >
+        <header className="App-header" >
+            <h2 > Hello this is Hasan. </h2> 
+        </header> 
+        </div>
+    );
+}
+
+export default App;
